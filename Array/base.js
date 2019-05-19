b233f4caa29f3aa1ea83f896adf06ba9d18b10ff
zhuxingyu <1667582186@qq.com>
2019-05-19 10:19:45 +0800

    js功能扩展

---
+++ Array/base.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @constructor 
+ */
+
+class ArrayExtend{
+
+    constructor(){
+
+    }
+    
+   /**
+    *  检测基本数据类型数据集合是否包含某项基本类型数据
+    */
+   contains(target){
+           
+   }
+}
